2bca4f7efbad890f573b0ef02cc180a44508eaad
Tirupathi1729 <kakarlatirupathireddy@gmail.com>
2023-07-25 20:27:35 +0530

    Arithmetic operators

--- 07-arithmetic.sh
+++ 07-arithmetic.sh
@@ -2,4 +2,19 @@ echo "enter a number"
 read num1
 echo "enter a number"
 read num2
-echo "Addition of two namers is $((num1+num2))"
+echo "Addition of two numbers is $((num1+num2))"
+echo "substraction of two numbers is $((num1-num2))"
+echo "$((num1*num2))"
+echo "$((num1/num2))"
+echo "$((num1//num2))"
+echo "$((num1%num2))"
+echo "$((num1++))"
+echo "$((++num1))"
+echo "$((num1--))"
+echo "$((--num1))"
+
+
+
+
+
+
